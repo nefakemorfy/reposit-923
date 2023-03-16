621a69411c4cf2e7306b8b0e8475bce915b1327c
nefakemorfy <125242983+nefakemorfy@users.noreply.github.com>
2023-03-16 12:47:20 +0200

    версия 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 8, -1, 4, 16, -5, 3 };
+            int max = array[0]; int indexMax = 0; int indexMin = 0; int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    indexMax = i;
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    indexMin = i;
+                }
+
+            }
+            Console.WriteLine($"min={min}\tindexMin={indexMin}");
+            Console.WriteLine($"max={max}\tindexMax={indexMax}");
+            Console.Read();
+        }
+    }
+}
